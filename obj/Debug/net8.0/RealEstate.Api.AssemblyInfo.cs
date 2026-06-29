2b99501ee0de4857d81d35a452e7b6c941652b99
Your Full Name <ali@gmail.com>
2026-06-29 14:11:32 +0330

    Add complete RealEstate API source code

--- obj/Debug/net8.0/RealEstate.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealEstate.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a192b292a12eb5be5909e64fc6a65dc3767f913b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
